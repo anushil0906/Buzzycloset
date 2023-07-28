47cfc5ce7d658b8378126365e7ba552862ea955b
anushil0906 <57445937+anushil0906@users.noreply.github.com>
2023-07-28 10:48:57 -0400

    Update settings.py

--- digt4tees/digi/digi/settings.py
+++ digt4tees/digi/digi/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-3!%6%26hch&&z70&fagk(lnut53p^8yarnwy-0-)1z0^s#8&)%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.9','127.0.0.1','.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
